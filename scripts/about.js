c1808cca3da418e74a76ca44acc642e537ffc60a
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-06-22 17:13:09 +0300

    Conduct linters checks and fix linters errors

--- scripts/about.js
+++ scripts/about.js
@@ -1,6 +1,4 @@
 // MENU BAR ON MOBILE
-console.log('hooked')
-
 const menuBarOverlay = document.querySelector('#menu-bar-overlay');
 const closeMenu = document.querySelector('#close-menu-item');
 const openMenu = document.querySelector('#hamburger-menu');
@@ -8,28 +6,27 @@ const menuOptions = document.querySelectorAll('.menu-options');
 // view menu list
 
 const viewMenuItems = () => {
-    openMenu.addEventListener('click', () => {
-        menuBarOverlay.style.display = 'block';
-    })
-}
+  openMenu.addEventListener('click', () => {
+    menuBarOverlay.style.display = 'block';
+  });
+};
 viewMenuItems();
 
 // close menu list
 
 const closeMenuItems = () => {
-    closeMenu.addEventListener('click', () => {
-        menuBarOverlay.style.display = 'none';
-    })
-}
+  closeMenu.addEventListener('click', () => {
+    menuBarOverlay.style.display = 'none';
+  });
+};
 closeMenuItems();
 
 // Select menu options
 const openMenuOption = () => {
-    menuOptions.forEach((menuOption) => {
-        menuOption.addEventListener('click', () => {
-            menuBarOverlay.style.display = 'none';
-        })
-    })
-}
+  menuOptions.forEach((menuOption) => {
+    menuOption.addEventListener('click', () => {
+      menuBarOverlay.style.display = 'none';
+    });
+  });
+};
 openMenuOption();
-
