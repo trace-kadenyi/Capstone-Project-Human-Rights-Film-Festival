8a2e361c4d1ce987f7a334948c22afbaee7531f3
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-06-22 16:46:57 +0300

    Update the home and about sections as well as add javascript to create menu functionality

---
+++ scripts/about.js
@@ -0,0 +1,35 @@
+// MENU BAR ON MOBILE
+console.log('hooked')
+
+const menuBarOverlay = document.querySelector('#menu-bar-overlay');
+const closeMenu = document.querySelector('#close-menu-item');
+const openMenu = document.querySelector('#hamburger-menu');
+const menuOptions = document.querySelectorAll('.menu-options');
+// view menu list
+
+const viewMenuItems = () => {
+    openMenu.addEventListener('click', () => {
+        menuBarOverlay.style.display = 'block';
+    })
+}
+viewMenuItems();
+
+// close menu list
+
+const closeMenuItems = () => {
+    closeMenu.addEventListener('click', () => {
+        menuBarOverlay.style.display = 'none';
+    })
+}
+closeMenuItems();
+
+// Select menu options
+const openMenuOption = () => {
+    menuOptions.forEach((menuOption) => {
+        menuOption.addEventListener('click', () => {
+            menuBarOverlay.style.display = 'none';
+        })
+    })
+}
+openMenuOption();
+
